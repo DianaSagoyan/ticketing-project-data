1e13fba0e90b4864cfc8e2f636afd291537d01cb
DianaSagoyan <dianasagoyan@yahoo.com>
2022-11-01 16:11:49 -0500

    Added mappers

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -1,13 +1,19 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectDataApplication {
-
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
+
     }
 
 }
