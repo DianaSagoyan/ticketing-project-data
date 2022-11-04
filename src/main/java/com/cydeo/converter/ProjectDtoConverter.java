071a002a0bb5364464b7ae406717276d3049dc91
DianaSagoyan <dianasagoyan@yahoo.com>
2022-11-04 15:04:34 -0500

    Added projects servie/impl/repository/mapper

--- src/main/java/com/cydeo/converter/ProjectDtoConverter.java
+++ src/main/java/com/cydeo/converter/ProjectDtoConverter.java
@@ -2,28 +2,30 @@ package com.cydeo.converter;
 
 import com.cydeo.dto.ProjectDTO;
 //import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import com.cydeo.service.ProjectService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-//@Component
-//@ConfigurationPropertiesBinding
-//public class ProjectDtoConverter implements Converter<String, ProjectDTO> {
+@Component
+@ConfigurationPropertiesBinding
+public class ProjectDtoConverter implements Converter<String, ProjectDTO> {
 
-//    ProjectService projectService;
-//
-//    public ProjectDtoConverter(ProjectService projectService) {
-//        this.projectService = projectService;
-//    }
-//
-//    @Override
-//    public ProjectDTO convert(String source) {
-//
-//        if (source == null || source.equals("")) {
-//            return null;
-//        }
-//
-//        return projectService.findById(source);
-//
-//    }
+    ProjectService projectService;
 
-//}
+    public ProjectDtoConverter(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @Override
+    public ProjectDTO convert(String source) {
+
+        if (source == null || source.equals("")) {
+            return null;
+        }
+
+        return projectService.getByProjectCode(source);
+
+    }
+
+}
---
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cydeo.service.impl;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectServiceImpl {
+}
